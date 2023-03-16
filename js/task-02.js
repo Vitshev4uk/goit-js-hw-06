996b70b0ce6bfc8f594cad8ee8e77631e3a6bbb3
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-03-16 11:33:02 +0100

    update_task_2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+// console.log(list);
+
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement('li');
+  // console.log(listItem);
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  list.append(listItem);
+});
+
+
