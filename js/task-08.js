dce404a5fafef7f65bfe835b764c76f09e726b5e
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-03-19 15:26:57 +0100

    update_task_8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+const emailInput = form.querySelector('input[name="email"]');
+const passwordInput = form.querySelector('input[name="password"]');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (emailInput.value === '' || passwordInput.value === '') {
+        alert('Fields must be filled!')
+    } else {
+       let userData = {
+        email: emailInput.value,
+        password: passwordInput.value,
+       };
+        console.log(userData);
+        form.reset()
+    }
+})
