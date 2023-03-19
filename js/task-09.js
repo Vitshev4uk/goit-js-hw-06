b59e1b10d896bcbae58600abababd03d877be1f3
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-03-19 16:15:50 +0100

    update_task_9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const btnChangeCol = document.querySelector('.change-color');
+const textCol = document.querySelector('.color');
+
+btnChangeCol.addEventListener('click', (event) => {
+  body.style.backgroundColor = getRandomHexColor();
+  textCol.textContent = getRandomHexColor()
+});
